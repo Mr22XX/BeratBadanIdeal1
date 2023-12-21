6bdb4ecceb8356dee959de38c3fce852c06264a2
Rayhan Muhammad Adha <67584512+Mr22XX@users.noreply.github.com>
2023-12-21 21:32:56 +0700

    Add files via upload

---
+++ BBIdeal/src/bb.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Mr22xx
+ */
+public class bb {
+    public static void main(String[] args) {
+        new beratBadanIdeal().show();
+    }
+    
+}
